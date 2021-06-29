375adeae288f408b933ace88e1affa1d1cf4118e
abramov85 <85363609+abramov85@users.noreply.github.com>
2021-06-29 16:20:01 +0300

    Update task_2_2.py

--- Abramov_Evgene_dz_2/task_2_2.py
+++ Abramov_Evgene_dz_2/task_2_2.py
@@ -1,11 +1,21 @@
+# message_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+# edited_message_list = []
+# for idx, item in enumerate(message_list):
+#     if item.isdigit():
+#         edited_message_list.append(f'"{int(item):02}"')
+#     elif len(item) > 1 and item[1].isdigit():
+#         edited_message_list.append(f'"{item[:1]}{int(item[1]):02}"')
+#     else:
+#         edited_message_list.append(item)
+# print(edited_message_list)
+# print(' '.join(edited_message_list))
+
 message_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-edited_message_list = []
+print(id(message_list), message_list)
 for idx, item in enumerate(message_list):
     if item.isdigit():
-        edited_message_list.append(f'"{int(item):02}"')
+        message_list[idx] = f'"{int(item):02}"'
     elif len(item) > 1 and item[1].isdigit():
-        edited_message_list.append(f'"{item[:1]}{int(item[1]):02}"')
-    else:
-        edited_message_list.append(item)
-print(edited_message_list)
-print(' '.join(edited_message_list))
+        message_list[idx] = f'"{item[:1]}{int(item[1]):02}"'
+print(id(message_list), message_list)
+print(' '.join(message_list))
