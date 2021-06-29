41bb8eb8ca5ad7975cd484d6056c3d45913bfcc5
abramov85 <85363609+abramov85@users.noreply.github.com>
2021-06-29 14:09:18 +0300

    Update task_2_3.py

--- Abramov_Evgene_dz_2/task_2_3.py
+++ Abramov_Evgene_dz_2/task_2_3.py
@@ -5,17 +5,13 @@ n = 0
 while n < number_of_iterations:
     message_list.reverse()
     item = message_list.pop()
+    message_list.reverse()
     if item.isdigit():
-        message_list.reverse()
         message_list.append(f'"{int(item):02}"')
-        n += 1
     elif len(item) > 1 and item[1].isdigit():
-        message_list.reverse()
         message_list.append(f'"{item[:1]}{int(item[1]):02}"')
-        n += 1
     else:
-        message_list.reverse()
         message_list.append(item)
-        n += 1
+    n += 1
 print(id(message_list), message_list)
 print(' '.join(message_list))
